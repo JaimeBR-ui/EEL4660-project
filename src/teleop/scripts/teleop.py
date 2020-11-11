72d4b7d089570cf028f180cf5861857d3ade0ad5
JaimeBR-ui <bambas@knights.ucf.edu>
2020-11-11 00:53:15 +0000

    Added steering

--- src/teleop/scripts/teleop.py
+++ src/teleop/scripts/teleop.py
@@ -11,13 +11,23 @@ from std_msgs.msg import String
 io.setwarnings(False)
 io.setmode(io.BOARD)
 io.setup(7, io.OUT)
-p = io.PWM(7, 50)
-p.start(0)
+io.setup(16, io.OUT)
+
+drive = io.PWM(7, 50)
+drive.start(0)
+
+turn = io.PWM(16, 50)
+turn.start(0)
 
 def callback(data):
-	value = data.axes[1] * 4 + 7
-	p.ChangeDutyCycle(value)
-	print(value)
+	drive_v = data.axes[8] * 3 + 7
+	turn_v = -2 * data.axes[0] + 7
+	if abs(drive_v - 7) < 0.1:
+		drive_v = 0
+	if abs(turn_v - 7) < 0.1:
+		turn_v = 0
+	drive.ChangeDutyCycle(drive_v)
+	turn.ChangeDutyCycle(turn_v)
 
 def listener():
 	rospy.init_node('listener', anonymous=True)
